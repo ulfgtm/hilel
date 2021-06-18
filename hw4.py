23792cb574669b2d842ba9740d115928e67bc093
Ivan <ulfgtm3@gmail.com>
2021-06-18 16:23:43 +0300

    Hw Complete

---
+++ hw4.py
@@ -0,0 +1,55 @@
+#1
+
+value_int=int(input('ведите число:'))
+new_value_int = value_int / 2 if value_int < 100 else value_int
+print(new_value_int)
+
+
+#####################
+#2
+
+value_int= int(input("Введите число:"))
+
+new_value_int= value_int = 1 if value_int < 100 else 0
+
+print(new_value_int)
+
+########################
+
+#3
+value_int= int(input("Введите число:"))
+
+new_value_int= value_int=True if value_int < 100 else False
+
+print(new_value_int)
+
+#######################
+
+#4
+my_str = input('введите текст:')
+my_str= my_str.upper()
+print(my_str)
+
+#######################
+
+#5
+my_str = input('ВВЕДИТЕ ТЕКСТ:')
+my_str= my_str.lower()
+print(my_str)
+#######################
+#6
+my_str= input("Введите строку:")
+my_str= my_str + my_str if len(my_str) < 5 else my_str
+print(my_str)
+#######################
+
+#7
+
+my_str= input('Введите текст:')
+my_str= my_str + my_str [::-1] if len(my_str) < 5 else my_str
+print(my_str)
+
+
+
+
+
