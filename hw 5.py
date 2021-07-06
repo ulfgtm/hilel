442e2931e66ed15dcee5782de68a8874bc0657e9
ivanfedosov <ulfgtm3@gmail.com>
2021-07-06 22:17:54 +0300

    Выполнил дз.

--- hw 5.py
+++ hw 5.py
@@ -1,71 +1,71 @@
 #1
-# value = 2032000
-# zero = 0
-# print(str(value).count(str(zero)))
+value = 2032000
+zero = 0
+print(str(value).count(str(zero)))
 
 
 #2
-# value = 10002000300000
-# new_value = value
-# value = len(str(value))
-# new_value = str(new_value)
-# new_value = new_value [::-1]
-# new_value = int(new_value)
-# new_value = len(str(new_value))
-# print(value - new_value)
+value = 10002000300000
+new_value = value
+value = len(str(value))
+new_value = str(new_value)
+new_value = new_value [::-1]
+new_value = int(new_value)
+new_value = len(str(new_value))
+print(value - new_value)
 
 #3
-# my_list_1 = ['H', 'o', 'l', 'l', 'a']
-# my_list_2 = ['W', 'o', 'r', 'l', 'd']
-#
-# my_result = []
-# for index, symbol in enumerate(my_list_1):
-#     if  index % 2:
-#         my_result.append(symbol)
-# for index, symbol in enumerate(my_list_2):
-#     if  not index % 2:
-#         my_result.append(symbol)
-#
-#
-# print(my_result)
+my_list_1 = ['H', 'o', 'l', 'l', 'a']
+my_list_2 = ['W', 'o', 'r', 'l', 'd']
 
-# #4
-# my_list_1 = ['H', 'o', 'l', 'l', 'a']
-# new_list = my_list_1.copy()[1:]
-# new_list.append(my_list_1[0])
-# print(new_list)
+my_result = []
+for index, symbol in enumerate(my_list_1):
+    if  index % 2:
+        my_result.append(symbol)
+for index, symbol in enumerate(my_list_2):
+    if  not index % 2:
+        my_result.append(symbol)
+
+
+print(my_result)
+
+#4
+my_list_1 = ['H', 'o', 'l', 'l', 'a']
+new_list = my_list_1.copy()[1:]
+new_list.append(my_list_1[0])
+print(new_list)
 
 
 
 #5
-# my_list = ['H', 'o', 'l', 'l', 'a']
-# my_list.append(my_list.pop(0))
-# print(my_list)
+my_list = ['H', 'o', 'l', 'l', 'a']
+my_list.append(my_list.pop(0))
+print(my_list)
 
 #6
-# my_str = "43 больше чем 34 но меньше чем 56"
-# summ = 0
-# my_str.split()
-# for i in my_str.split():
-#     if i.isdigit():
-#         summ = summ + int(i)
-# print(summ)
+my_str = "43 больше чем 34 но меньше чем 56"
+summ = 0
+my_str.split()
+for i in my_str.split():
+    if i.isdigit():
+        summ = summ + int(i)
+print(summ)
 
 #7
-# my_str = "Каааааакаято сстрооооооока"
-# r_limit = "а"
-# l_limit = "о"
-# sub_str = " "
-# sub_str= my_str[my_str.find(r_limit)+1:my_str.rfind(l_limit)]
-# print(sub_str)
+my_str = "Каааааакаято сстрооооооока"
+r_limit = "а"
+l_limit = "о"
+sub_str = " "
+sub_str= my_str[my_str.find(r_limit)+1:my_str.rfind(l_limit)]
+print(sub_str)
 
 #8
-#my_str = "qwerty"
-# my_list = []
-# if len(my_str) % 2:
-#     my_str = my_str + '_'
-# for index in range(len(my_str)//2):
-#     my_list.append(my_str[2 * index:2 * index + 2])
+my_str = "qwerty"
+my_list = []
+if len(my_str) % 2:
+    my_str = my_str + '_'
+for index in range(len(my_str)//2):
+    my_list.append(my_str[2 * index:2 * index + 2])
 
 #9
 my_list = [2,4,1,5,3,9,0,7]
